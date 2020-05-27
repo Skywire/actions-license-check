58d0180d1ca67b1a58ecaa7ebde8992678387435
Neil Aitken <neil@skywire.co.uk>
2020-05-27 21:16:52 +0100

    Implement license finder

--- license-finder.js
+++ license-finder.js
@@ -1,9 +1,34 @@
+const { readdirSync, statSync } = require('fs')
+const { join } = require('path')
+
 module.exports = {
     getDirs: function(namespaces) {
-        return [];
+        var directories = [];
+        namespaces.forEach(path => {
+            const dirs = readdirSync(path)
+            
+            dirs.forEach(filePath => {
+                if(statSync(path+'/'+filePath).isDirectory()) {
+                    directories.push(path+'/'+filePath);
+                }
+            });
+
+        });
+        
+        return directories;
     },
 
-    findMissingLicenses: function (dirs) {
-        return [];
+    findMissingLicenses: function (namespaces) {
+        const dirs = this.getDirs(namespaces);
+        var missing = [];
+
+        dirs.map(function(path, index) {
+            const files = readdirSync(path);
+            if(files.indexOf('LICENSE.txt') === -1) {
+                missing.push(path);
+            }
+        });
+
+        return missing;
     }
 }
--- test/index.js
+++ test/index.js
@@ -1,19 +1,33 @@
 var chai = require('chai');
 var licenseFinder = require('../license-finder.js')
 
+const fsmock = require('mock-fs');
+ 
+fsmock({
+  'app/code/Foo': {
+    'Foo1': {/** empty directory */},
+    'Foo2': {'LICENSE.txt': 'file content here',},
+  },
+  'app/code/Bar': {
+    'Bar1': {'LICENSE.txt': 'file content here',},
+    'Bar2': {/** empty directory */},
+  },
+});
+
 describe('License Finder', function () {
     describe('#getDirs', function () {
-        it('should return 2 directory paths', function () {
+        it('should return 4 directory paths', function () {
             let namespaces = ['app/code/Foo', 'app/code/Bar'];
             let dirs = licenseFinder.getDirs(namespaces);
-            chai.assert.lengthOf(dirs, 2)
+            chai.assert.lengthOf(dirs, 4)
         });
     });
 
     describe('#findMissingLicenses', function () {
-        it('should return 2 license file paths', function () {
+        it('should return 2 directory paths without LICENSE.txt files', function () {
             let missing = licenseFinder.findMissingLicenses(['app/code/Foo', 'app/code/Bar'])
             chai.assert.lengthOf(missing, 2)
+            chai.expect(["app/code/Foo/Foo1", "app/code/Bar/Bar2"]).to.eql(missing);
         });
     });
 });
